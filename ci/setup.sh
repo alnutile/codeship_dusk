5fad79cfba9d30fd394d4752ce3e6691922c6abb
alnutile <alfrednutile@gmail.com>
2017-07-09 23:14:48 -0400

    Show it all working

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,11 +3,12 @@
 set -e
 
 phpenv local 5.6
-composer install --no-interaction
+COMPOSER_HOME=${HOME}/cache/composer
+composer install --prefer-dist  --no-interaction
 
 ./vendor/laravel/dusk/bin/chromedriver-linux &
 
-cp .env.testing .env
+cp .env.codeship .env
 
 php artisan serve > /dev/null 2>&1 &
 sleep 3
