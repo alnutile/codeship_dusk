65bd6410484d0a4124ac4b9024ab214943a58a2e
alnutile <alfrednutile@gmail.com>
2017-07-09 23:12:26 -0400

    Show it all working

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,5 +5,9 @@ set -e
 phpenv local 5.6
 composer install --no-interaction
 
+./vendor/laravel/dusk/bin/chromedriver-linux &
 
-php artisan serve --quiet
+cp .env.testing .env
+
+php artisan serve > /dev/null 2>&1 &
+sleep 3
